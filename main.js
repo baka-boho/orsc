5128958051cab4ff4c00bdcb954b0dc98eb20540
billel <benabdallahbillel5@gmail.com>
2023-04-06 02:37:49 +0100

    added left and right svcroll button for card lists

--- main.js
+++ main.js
@@ -36,16 +36,95 @@ function openQuestion(nbr) {
 // }
 // document.getElementById('event-content').scrollBy();
 
-function toggleModel(text){
-  var model=document.getElementById(text)
+function toggleModel(text) {
+  var model = document.getElementById(text)
   model.classList.toggle("hidden")
   model.classList.toggle("visible")
   // console.log(text)
 }
 
-function toggleEvent(text){
-  var card=document.getElementById(text)
+function toggleEvent(text) {
+  var card = document.getElementById(text)
   // console.log(card);
   card.firstElementChild.classList.toggle("hidden")
+}
+
+
+events = document.getElementById('event-content')
+events = events.children
+projects = document.getElementById('project-content')
+projects = projects.children
+console.log(events)
+console.log(projects)
+
+let event_options = {
+  root: document.querySelector("#event-content"),
+  rootMargin: "0px",
+  threshold: 0.9,
+};
+let project_options = {
+  root: document.querySelector("#project-content"),
+  rootMargin: "0px",
+  threshold: 0.9,
+};
+var event_vis = {}
+var project_vis = {}
+
+let Event_callback = (entries) => {
+  entries.forEach((entry) => {
+    event_vis[entry.target.id] = entry.isIntersecting
+  });
+}
+let Project_callback = (entries) => {
+  entries.forEach((entry) => {
+    project_vis[entry.target.id] = entry.isIntersecting
+  });
+};
+let Event_observer = new IntersectionObserver(Event_callback, event_options);
+let Project_observer = new IntersectionObserver(Project_callback, project_options);
+
+for (let i = 0; i < events.length; i++) {
+  Event_observer.observe(events[i])
+}
+for (let i = 0; i < projects.length; i++) {
+  Project_observer.observe(projects[i])
+}
+
+// events
+// console.log(event);
+// })
+function slide(a, b) {
+  if(a==0){
+    vis=event_vis
+  }
+  if(a==1){
+    vis=project_vis
+  }
+  left = Object.values(vis).indexOf(true)
+  right =left+2
+  first_id = Object.keys(vis).at(left)
+  last_id = Object.keys(vis).at(right)
+console.log(left,first_id,right,last_id)
+console.log('b',b,'l',)  
+if(b==1){
+    if(right>=Object.values(vis).length-1){
+      slide(a,-1)
+    }else{
+      target=document.getElementById(Object.keys(vis).at(right+1))
+      console.log('=============')
+      console.log(target)
+      target.scrollIntoView({ behavior: "smooth",block:"center", inline: "nearest" });
+    }
+  }
+  if(b==-1){
+    if(left<=0){
+      slide(a, 1)
+    }else{
+      target=document.getElementById(Object.keys(vis).at(left-1))
+      console.log('xxxxxxxxxxxxxxxxx')
+      console.log(target)
+      target.scrollIntoView({ behavior: "smooth",block:"center", inline: "nearest" });
+    }
+  }
 }
 
